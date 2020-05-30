361e63596fb426f51d038860672a57aef366b2a7
LeoSirius <is.li.xiaoyu@qq.com>
2020-05-30 15:38:04 +0800

    update

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         <h2 className="mb-5 maroon-text">I'm Leo Sirius, a senior markdown engineer :)</h2>
         <p className="mt-5 font-weight-bold">You can contact me through following ways</p>
         <ul>
-          <li><span className="font-weight-bold">Emails: </span>is.li.xiaoyu@qq.com, leosirius666@gmail.com (choose one, both works)</li>
+          <li><span className="font-weight-bold">Emails: </span>is.li.xiaoyu@qq.com, leosirius666@gmail.com (choose one, both work)</li>
           <li><span className="font-weight-bold">Github: </span><a target="_blank" href="https://github.com/LeoSirius">https://github.com/LeoSirius</a></li>
         </ul>
         <div>
